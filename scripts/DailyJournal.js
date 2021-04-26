85e7c044fb193e20eec976c3e4f91bbf01d13f9c
alambrow <alexander.lambrow@gmail.com>
2021-04-26 14:49:15 -0500

    updated html and css

--- scripts/DailyJournal.js
+++ scripts/DailyJournal.js
@@ -2,8 +2,23 @@ import { Entries } from "./Entries.js"
 
 export const DailyJournal = () => {
     return `
+        <h1>Daily Journal</h1>
+
         <div class="entryList">
             ${ Entries() }
         </div>
+
+        <article class="entryForm">
+            Use interpolation to put the HTML that the form function generates here
+        </article>
     `
 }
+
+
+// export const DailyJournal = () => {
+//     return `
+//         <div class="entryList">
+//             ${ Entries() }
+//         </div>
+//     `
+// }
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
 import { DailyJournal } from "./DailyJournal.js"
+import { Entries } from "./Entries.js"
 
-const container = document.querySelector("#entries")
+const entry_container = document.querySelector("#container")
+const oldEntry_container = document.querySelector("#old__entries")
 
 const render = () => {
-    container.innerHTML = DailyJournal()
+    entry_container.innerHTML = DailyJournal()
+    oldEntry_container.innerHTML = Entries()
 }
+
+render();
